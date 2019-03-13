c16a45b1afb6d5b1decc876a0bbeb3fb7b14fcb8
Israel Salinas Martínez <israel.salinas.m@gmail.com>
2019-03-13 11:29:07 -0600

    first commit

---
+++ js/dbContent.js
@@ -0,0 +1,34 @@
+var dbContent = [
+	{
+		countryName:"España",
+		capital:"Madrid",
+		continent:"Europa",
+		population:"46,397,452",
+		imgUrl:"img/flag-spain.png"
+	},
+	{
+		countryName:"China",
+		capital:"Pekín",
+		continent:"Asia",
+		population:"1,415,045,928",
+		imgUrl:"img/flag-china.png"
+	},{
+		countryName:"México",
+		capital:"CDMX",
+		continent:"América",
+		population:"13,759,074",
+		imgUrl:"img/flag-mexico.png"
+	},{
+		countryName:"Nigeria",
+		capital:"Abuya",
+		continent:"África",
+		population:"195,875,237",
+		imgUrl:"img/flag-nigeria.png"
+	},{
+		countryName:"Australia",
+		capital:"Camberra",
+		continent:"Oceanía",
+		population:"24,772,247",
+		imgUrl:"img/flag-australia.png"
+	}
+]
